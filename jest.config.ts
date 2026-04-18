a5d53d716bf5ee6b8da144f8245f3ccc800b312c
kevin verzosa <kevinverzosa@kevins-MacBook-Pro-2.local>
2026-04-17 23:59:31 -0500

    fix: move sanity mock to root __mocks__ and fix jest moduleNameMapper merge

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,21 @@ import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({ dir: './' })
 
-const config: Config = {
+const baseConfig: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^sanity$': '<rootDir>/src/__mocks__/sanity.ts',
-  },
 }
 
-export default createJestConfig(config)
+const jestConfig = createJestConfig(baseConfig)
+
+export default async () => {
+  const config = await jestConfig()
+  return {
+    ...config,
+    moduleNameMapper: {
+      ...config.moduleNameMapper,
+      '^sanity$': '<rootDir>/__mocks__/sanity.ts',
+    },
+  }
+}
